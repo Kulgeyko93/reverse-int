cc56b40468bb9c8081baf6ba663763589e749880
Aleksey Kulgeyko <kulgeyko93@mail.ru>
2020-10-19 17:54:02 +0300

    docs: complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result = [...`${n}`].reverse();
+  if ( result[result.length -1] === '-') {
+    result.pop();
+  }
+  return Number(result.join(''))
+
 }
